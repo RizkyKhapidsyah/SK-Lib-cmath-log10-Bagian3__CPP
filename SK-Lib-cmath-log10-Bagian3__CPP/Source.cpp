5ea5279a07b5e4b63efa63f31bd44411d6eb04d4
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-06-21 11:15:42 +0700

    Add project files.

---
+++ SK-Lib-cmath-log10-Bagian3__CPP/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+/*	Source by Programiz
+	Modified For Learn by Rizky Khapidsyah
+	I.D.E : VS2019 
+*/
+
+using namespace std;
+
+int main() {
+	int x = 2;
+	double result;
+
+	result = log10(x);
+	cout << "log10(x) = " << result << endl;
+
+	_getch();
+	return 0;
+}
